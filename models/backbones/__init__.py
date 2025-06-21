cc291c00f1836d30219b392f1f5318068bae680f
yuecg <112631541+yuecg@users.noreply.github.com>
2025-06-21 22:29:25 +0800

    Delete models directory

--- models/backbones/__init__.py
+++
@@ -1,7 +0,0 @@
-from .bp2mn import BP2MN
-from .beit_baseline import BEiTBaseline
-from .vit_baseline import ViTBaseline
-
-__all__ = ['BP2MN']
-
-# __all__ = ['ViTBaseline', 'BEiTBaseline', 'BP2MN']
