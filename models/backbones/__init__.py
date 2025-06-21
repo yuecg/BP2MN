9f3204b198d984e1a2447c55305ca598210b70a5
yuecg <112631541+yuecg@users.noreply.github.com>
2025-06-21 22:28:07 +0800

    Add files via upload

---
+++ models/backbones/__init__.py
@@ -0,0 +1,7 @@
+from .bp2mn import BP2MN
+from .beit_baseline import BEiTBaseline
+from .vit_baseline import ViTBaseline
+
+__all__ = ['BP2MN']
+
+# __all__ = ['ViTBaseline', 'BEiTBaseline', 'BP2MN']
